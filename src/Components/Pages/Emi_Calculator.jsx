25dd42d1c7f72bf1a93ea0e2d422b46dd8c1a960
subrat tiwari <salaryfundnow@gmail.com>
2026-08-04 18:39:51 +0530

    Initial commit

--- src/Components/Pages/Emi_Calculator.jsx
+++ src/Components/Pages/Emi_Calculator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import "./../css/Emi_Calculator.css";
+import "../../css/emi_calculator.css";
 
 const Emi_Calculator = () => {
   const [loanAmount, setLoanAmount] = useState("");
